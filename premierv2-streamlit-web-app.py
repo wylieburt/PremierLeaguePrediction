f3ba6a2da8184b577ecef806be92b3b198c70607
wylieburt <wylierburt@gmail.com>
2025-09-04 00:27:19 -0700

    updated location of images

--- premierv2-streamlit-web-app.py
+++ premierv2-streamlit-web-app.py
@@ -17,7 +17,7 @@ tab1, tab2 = st.tabs(["Make Prediction", "About"])
 # Images to spruce things up
 
 with tab1:
-    st.image("images/crystal_palace_stadium.jpg")
+    st.image("crystal_palace_stadium.jpg")
 
     # Add title and instructions
     st.title("Premier League Match Prediction")
@@ -362,7 +362,7 @@ with tab1:
         left.subheader(f"Pick: {pick}")
         
 with tab2:
-    st.image("images/crystal_palace_stadium.jpg")
+    st.image("crystal_palace_stadium.jpg")
 
     # Read and display markdown file
     with open("prediction_web.md", "r") as file:
