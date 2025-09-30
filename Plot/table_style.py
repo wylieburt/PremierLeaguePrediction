3f445e25e4012bab8c612356dbbd350d411dde39
wylieburt <wylierburt@gmail.com>
2025-09-29 17:38:43 -0700

    Add files via upload

--- Plot/table_style.py
+++ Plot/table_style.py
@@ -1,8 +1,21 @@
+import pandas as pd
+
 def highlight_multiple_conditions(row):
     """Apply different colors based on prediction being correct or not"""
+    if pd.isna(row['Result']):
+        return ['background-color: transparent'] * len(row) # No color
     if row['Result'] == row['Predicted']:
         return ['background-color: #08f10a'] * len(row)  # Green
     elif row['Result'] != row['Predicted']:
         return ['background-color: #f13c08'] * len(row)  # Red
     else:
         return [''] * len(row)
+    
+def highlight_max(row):
+    # Create a Series of empty strings with the same index as the row
+    styles = [''] * len(row)
+    # Find the index of the maximum value
+    max_idx = row.argmax()
+    # Set the style for the max value
+    styles[max_idx] = 'background-color: lightgreen'
+    return styles
